56f18e3121a7941a82a8f362d4100378f4a57547
George Testo <xghostvrx@gmail.com>
2024-05-29 12:51:22 -0700

    Add user login functionality to /api/users/login endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -50,6 +50,33 @@ app.post('/api/users/register', async (req, res) => {
   }
 });
 
+// Login
+app.post('/api/users/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  // Validation
+  if (!(email && password)) {
+    return res.status(400).send('All fields are required.');
+  }
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email }
+    });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).send('Invalid credentials.');
+    }
+
+    // Create JWT token
+    const token = jwt.sign({ userId: user.id}, process.env.JWT_SECRET!, { expiresIn: '1h' });
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).send('Server error');
+  }
+});
+
 app.listen(8080, () => {
   console.log('Server is running on port 8080');
 });
