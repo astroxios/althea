6ae16a5071c329274f3e568a2a0abf14e8adb07e
George Testo <xghostvrx@gmail.com>
2024-05-29 11:48:47 -0700

    Add user registration functionality to /api/users endpoint

---
+++ server/src/index.ts
@@ -0,0 +1,55 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client'
+import dotenv from 'dotenv';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+// Load environment variables
+dotenv.config();
+
+const prisma = new PrismaClient();
+const app = express();
+app.use(express.json());
+
+// Registration
+app.post('/api/users', async (req, res) => {
+  const { username, email, password } = req.body;
+
+  // Validation
+  if (!(username && email && password)) {
+    return res.status(400).send('All fields are required.');
+  }
+
+  try {
+    const existingUser = await prisma.user.findUnique({
+      where: { username } || { email }
+    });
+
+    if (existingUser) {
+      return res.status(409).send('User already exists.');
+    }
+
+    // Pasword Hashing
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const user = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    // Create JWT token
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET!, { expiresIn: '1h'});
+
+    res.status(201).json({ token });
+  } catch (error) {
+    res.status(500).send('Server error');
+  }
+});
+
+app.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
