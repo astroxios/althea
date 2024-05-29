11eabf6fded1475c689209e4e3fe24127dccdec6
Schrixx <fun_nos@yahoo.com>
2024-05-29 10:17:21 -0700

    Add initial project files and folder structures

---
+++ client/src/features/__test__/index.tsx
@@ -0,0 +1,1 @@
+// central export point for all features
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './assets/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
 )
