4e9ad2f0a2451fc97256e75b1dd577f85a9a6213
Moses Osuna <xschrixx@gmail.com>
2024-05-30 05:31:19 -0700

    Add absolute pathing to vite/typescript configs

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: "/src",
+      assets: "/src/assets",
+      data: "/src/data",
+      components: "/src/components",
+      layouts: "/src/components/layouts",
+      pages: "/src/pages",
+      types: "/src/types",
+      services: "/src/services",
+      features: "/src/features",
+    },
+  },
 })
