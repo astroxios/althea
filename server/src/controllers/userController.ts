6f72131b0655bc7c032c23e14116c8ac3addb00f
George Testo <xghostvrx@gmail.com>
2024-06-06 20:18:05 -0700

    fix: check user in patchUserDetails

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -32,12 +32,10 @@ export const registerUser = async (req: Request, res: Response) => {
 export const getUser = async (req: Request, res: Response) => {
     const { id } = req.params;
     try {
-
         const user = await getUserById(Number(id));
         if (!user) {
             return res.status(404).json({ error: 'User not found' });
         }
-
         // Exclude specific properties from the response
         const filteredResponse = filterProperties(user, [
             'email',
@@ -45,12 +43,10 @@ export const getUser = async (req: Request, res: Response) => {
             'createdAt',
             'updatedAt'
         ]);
-
         res.status(200).json({
             message: 'User retrieval successful',
             data: [filteredResponse]
         });
-
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
     }
@@ -60,14 +56,13 @@ export const patchUserDetails = async (req: Request, res: Response) => {
     const { id } = req.params;
     const data = req.body;
     try {
-        const user = await updateUser(Number(id), data);
+        const user = await getUserById(Number(id));
         if (!user) {
             return res.status(404).json({ error: 'User not found' });
         }
-
+        const updated_user = await updateUser(Number(id), data);
         // Exclude sensitive properties from the response
-        const redactedResponse = redactSensitiveProperties(user, ['password']);
-
+        const redactedResponse = redactSensitiveProperties(updated_user, ['password']);
         res.status(200).json({
             message: 'User update successful',
             data: [redactedResponse]
