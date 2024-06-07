1d6ba22dd4d08facda1fb5a703dcd00b817f309d
George Testo <xghostvrx@gmail.com>
2024-06-06 20:25:01 -0700

    test: validationMiddleware

--- server/tests/mockApp.ts
+++ server/tests/mockApp.ts
@@ -1,11 +1,21 @@
 import express from 'express';
 import { authenticateToken } from '../src/middleware/auth';
+import { registerValidationRules, patchValidationRules } from '../src/middleware/validation';
+import { handleValidationErrors } from '../src/middleware/handleValidationErrors';
 
 const mockApp = express();
 
 mockApp.use(express.json());
 
-mockApp.get('/protected', authenticateToken, (req, res) => {
+mockApp.post('/register', registerValidationRules(), handleValidationErrors, (req: express.Request, res: express.Response) => {
+    res.status(200).json({ message: 'Registration successful' });
+});
+
+mockApp.patch('/update', patchValidationRules(), handleValidationErrors, (req: express.Request, res: express.Response) => {
+    res.status(200).json({ message: 'Update successful' });
+});
+
+mockApp.get('/protected', authenticateToken, (req: express.Request, res: express.Response) => {
     res.status(200).json({ id: req.user.id, email: req.user.email });
 });
 
