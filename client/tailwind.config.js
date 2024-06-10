825417a1aa24b8077cdb9b07437087e968369ae9
Moses Osuna <xschrixx@gmail.com>
2024-06-10 08:38:07 -0700

    Refactor configs

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        loginBackground: "#F0F5FF",
+        athenaBlue: "#5EE0FE",
+        blueHeath:"#5863F8",
+        raisinBlack:"#2B2B2F",
+        vanDyke: "#484041",
+        lightGreen: "#70EE9C",
+        oceanBlue: "#2F85FC",
+        rose: "#FF6232",
+      }
+    },
   },
   plugins: [],
 }
