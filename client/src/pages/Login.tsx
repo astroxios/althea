d572a76422237823f2fff5c29f5d316b6fdaf063
Moses Osuna <xschrixx@gmail.com>
2024-06-10 08:40:38 -0700

    Add page components

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,16 @@
+import { LoginForm } from "features/loginRegister"
+import Footer from "components/layouts/Footer"
+import SimpleNavbar from "components/layouts/SimpleNavbar"
+
+const Login = () => {
+  return (
+    <div className="relative h-screen w-full flex flex-col items-center">
+      <div className="bg-loginBackground -z-20 absolute h-screen w-screen" />
+      <SimpleNavbar />
+      <LoginForm />
+      <Footer />
+    </div>
+  )
+}
+
+export default Login
