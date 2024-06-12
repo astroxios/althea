7bed4c3930f5ba2da98f9b77b30e90ce77b47fff
George Testo <metro1102@gmail.com>
2024-06-12 13:10:23 -0700

    refactor: apply migrations on server startup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,36 @@
 import app from './app';
+import { PrismaClient } from '@prisma/client';
+import { exec } from 'child_process';
+import util from 'util';
+
+const execPromise = util.promisify(exec);
+const prisma = new PrismaClient();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function applyMigrations() {
+  try {
+    console.log('Applying migrations...');
+    await execPromise('bunx prisma migrate deploy');
+    console.log('Migrations applied successfuly.');
+  } catch(error) {
+    console.error('Error applying migraitons:', error);
+  }
+}
+
+async function main() {
+  await applyMigrations();
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+main()
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  })
