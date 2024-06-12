c5e3846775ff83d8b7cee665bf1f9101b363b297
George Testo <xghostvrx@gmail.com>
2024-06-12 11:14:20 -0700

    add: authenticate to 'widgets' routes

--- server/src/routes/widgetRoutes.ts
+++ server/src/routes/widgetRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import * as widgetController from '../controllers/widgetController';
+import { authenticate } from '../middleware/authMiddleware';
 
 const router = Router();
 
-router.get('/widgets', widgetController.getWidgets);
-router.post('/widgets', widgetController.createWidget);
-router.put('/widgets/:id', widgetController.updateWidget);
-router.delete('/widgets/:id', widgetController.deleteWidget);
+router.get('/widgets', authenticate, widgetController.getWidgets);
+router.post('/widgets', authenticate, widgetController.createWidget);
+router.put('/widgets/:id', authenticate, widgetController.updateWidget);
+router.delete('/widgets/:id', authenticate, widgetController.deleteWidget);
 
 export default router;
