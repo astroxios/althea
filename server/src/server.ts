906a1329d52bf2213f059762e0cf50930778e649
George Testo <metro1102@gmail.com>
2024-06-18 08:33:25 -0700

    chore: check for pending migrations before applying

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,13 +8,31 @@ const prisma = new PrismaClient();
 
 const PORT = process.env.PORT || 3000;
 
+async function checkPendingMigrations() {
+  try {
+    const { stdout } = await execPromise('bunx prisma migrate status --schema=./prisma/schema.prisma');
+    return stdout.includes('Database schema is not in sync with the Prisma schema.');
+  } catch (error) {
+    console.error('Error checking migration status:', error);
+    throw error;
+  }
+}
+
 async function applyMigrations() {
   try {
-    console.log('Applying migrations...');
-    await execPromise('bunx prisma migrate deploy');
-    console.log('Migrations applied successfuly.');
+    console.log('Checking for pending migrations...');
+    const hasPendingMigrations = await checkPendingMigrations();
+
+    if (hasPendingMigrations) {
+      console.log('Applying migrations...');
+      await execPromise('bunx prisma migrate deploy');
+      console.log('Migrations applied successfuly.');
+    } else {
+      console.log('No pending migrations.');
+    }
   } catch(error) {
     console.error('Error applying migrations:', error);
+    throw error;
   }
 }
 
