4810e35c5ecd27e2809c7fadb7237ba2f71a9bdd
George Testo <metro1102@gmail.com>
2024-06-18 17:56:42 -0700

    fix: cross origin compatibility

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,10 +3,13 @@ import authRoutes from './routes/authRoutes';
 import userRoutes from './routes/userRoutes';
 import dotenv from 'dotenv';
 import path from 'path';
+import cors from 'cors';
 
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use('/api/auth', authRoutes)
