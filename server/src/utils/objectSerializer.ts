c47b763169b733adb54a49941481235eb0a4d1b7
George Testo <metro1102@gmail.com>
2024-06-20 16:01:49 -0700

    docs: annotate object serialization utility

--- server/src/utils/objectSerializer.ts
+++ server/src/utils/objectSerializer.ts
@@ -1,5 +1,21 @@
 import { filterProperties, redactSensitiveProperties } from './filterProperties';
 
+/**
+ * Serializes an object by excluding specified properties and optionally replacing sensitive properties.
+ *
+ * @param {string} type - The type of the object being serialized. This could be used for logging or other type-specific behavior.
+ * @param {any} obj - The object to be serialized.
+ * @param {string[]} [propertiesToExclude=[]] - An array of property names to exclude from the serialized object.
+ * @param {string[]} [sensitiveProperties=[]] - An array of property names that are considered sensitive. These properties could be replaced or transformed in the serialized object.
+ *
+ * @returns {any} - The serialized object.
+ *
+ * @example
+ * const user = { id: 1, name: 'John Doe', password: 'secret' };
+ * const serializedUser = serializeObject('user', user, ['password'], ['password']);
+ * console.log(serializedUser); // { id: 1, name: 'John Doe' }
+ */
+
 export const serializeObject = (
     type: string,
     obj: any,
